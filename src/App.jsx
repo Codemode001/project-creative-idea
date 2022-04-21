229ae80447b3b7a2579e09602089b1876eb2123b
Codemode001 <haroldrex12331@gmail.com>
2022-04-22 02:32:27 +0800

    update in the homepage and login page

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,19 @@
 import React from "react";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+
 import Signup from "./pages/signup";
-import SignIn from "./pages/login";
+import LogIn from "./pages/login";
 import Dashboard from "./pages/homepage";
 
 const App = () => {
   return (
-    <>
-      <Signup />
-    </>
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/sign-up" component={Signup} />
+        <Route exact path="/dashboard" component={Dashboard} />
+        <Route path="/" component={LogIn} />
+      </Switch>
+    </BrowserRouter>
   );
 };
 
