a703174e0699076dab2786a5e8ffcae764835155
Codemode001 <haroldrex12331@gmail.com>
2022-04-27 01:26:46 +0800

    Revert "update"
    This reverts commit 1b38fecec0e02c99248b659921f93cdb33f49f2c.

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,17 @@ import { BrowserRouter, Route, Switch } from "react-router-dom";
 import Signup from "./pages/signup";
 import LoginPage from "./pages/login";
 import Dashboard from "./pages/homepage";
-import Profile from "./pages/profile";
 
 const App = () => {
-  return <Profile />;
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/sign-up" component={Signup} />
+        <Route exact path="/dashboard" component={Dashboard} />
+        <Route path="/" component={LoginPage} />
+      </Switch>
+    </BrowserRouter>
+  );
 };
 
 export default App;
