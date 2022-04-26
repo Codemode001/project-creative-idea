1b38fecec0e02c99248b659921f93cdb33f49f2c
Codemode001 <haroldrex12331@gmail.com>
2022-04-27 01:22:27 +0800

    update

--- src/App.jsx
+++ src/App.jsx
@@ -4,17 +4,10 @@ import { BrowserRouter, Route, Switch } from "react-router-dom";
 import Signup from "./pages/signup";
 import LoginPage from "./pages/login";
 import Dashboard from "./pages/homepage";
+import Profile from "./pages/profile";
 
 const App = () => {
-  return (
-    <BrowserRouter>
-      <Switch>
-        <Route exact path="/sign-up" component={Signup} />
-        <Route exact path="/dashboard" component={Dashboard} />
-        <Route path="/" component={LoginPage} />
-      </Switch>
-    </BrowserRouter>
-  );
+  return <Profile />;
 };
 
 export default App;
