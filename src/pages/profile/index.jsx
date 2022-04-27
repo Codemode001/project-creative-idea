2f0608a863807decf1a39108319e5c47ef8e3cb2
Codemode001 <haroldrex12331@gmail.com>
2022-04-27 11:32:15 +0800

    update on the profile page

--- src/pages/profile/index.jsx
+++ src/pages/profile/index.jsx
@@ -49,59 +49,61 @@ export default function ProfilePage() {
     setValue(newValue);
   };
   return (
-    <Container>
-      <Stack style={{ alignItems: "center" }}>
-        <Avatar
-          alt="Remy Sharp"
-          src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/94/Robert_Downey_Jr_2014_Comic_Con_%28cropped%29.jpg/1200px-Robert_Downey_Jr_2014_Comic_Con_%28cropped%29.jpg"
-          style={{ width: "150px", height: "150px" }}
-        />
-        <h1 style={{ fontFamily: " Neue Helvetica" }}>Robert Downey Jr.</h1>
-        <span style={{ color: "gray" }}>@ironman</span>
-        <div>
-          <Button
-            variant="contained"
-            component="span"
-            style={{ marginRight: "1rem", marginTop: "1rem" }}
-          >
-            Share
-          </Button>
-          <Button
-            variant="contained"
-            component="span"
-            style={{ marginTop: "1rem" }}
-          >
-            Edit Profile
-          </Button>
-        </div>
-        <Box sx={{ width: "100%" }}>
-          <Box sx={{ borderBottom: 1, borderColor: "divider" }}>
-            <div style={{ marginTop: "2rem", marginLeft: "30rem" }}>
-              <Tabs
-                value={value}
-                onChange={handleChange}
-                aria-label="basic tabs example"
-              >
-                <Tab label="Created" {...a11yProps(0)} />
-                <Tab label="Saved" {...a11yProps(1)} />
-              </Tabs>
+    <React.Fragment>
+      <Container>
+        <Stack style={{ alignItems: "center" }}>
+          <Avatar
+            alt="Remy Sharp"
+            src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/94/Robert_Downey_Jr_2014_Comic_Con_%28cropped%29.jpg/1200px-Robert_Downey_Jr_2014_Comic_Con_%28cropped%29.jpg"
+            style={{ width: "150px", height: "150px" }}
+          />
+          <h1 style={{ fontFamily: " Neue Helvetica" }}>Robert Downey Jr.</h1>
+          <span style={{ color: "gray" }}>@ironman</span>
+          <div>
+            <Button
+              variant="contained"
+              component="span"
+              style={{ marginRight: "1rem", marginTop: "1rem" }}
+            >
+              Share
+            </Button>
+            <Button
+              variant="contained"
+              component="span"
+              style={{ marginTop: "1rem" }}
+            >
+              Edit Profile
+            </Button>
+          </div>
+          <Box sx={{ width: "100%" }}>
+            <Box sx={{ borderBottom: 1, borderColor: "divider" }}>
+              <div style={{ marginTop: "2rem", marginLeft: "30rem" }}>
+                <Tabs
+                  value={value}
+                  onChange={handleChange}
+                  aria-label="basic tabs example"
+                >
+                  <Tab label="Created" {...a11yProps(0)} />
+                  <Tab label="Saved" {...a11yProps(1)} />
+                </Tabs>
+              </div>
+            </Box>
+            <div
+              style={{
+                marginLeft: "25rem",
+                marginTop: "5rem",
+              }}
+            >
+              <TabPanel value={value} index={0}>
+                <h1>No created Pins yet</h1>
+              </TabPanel>
+              <TabPanel value={value} index={1}>
+                <h1>No created Pins yet</h1>
+              </TabPanel>
             </div>
           </Box>
-          <div
-            style={{
-              marginLeft: "25rem",
-              marginTop: "5rem",
-            }}
-          >
-            <TabPanel value={value} index={0}>
-              <h1>No created Pins yet</h1>
-            </TabPanel>
-            <TabPanel value={value} index={1}>
-              <h1>No created Pins yet</h1>
-            </TabPanel>
-          </div>
-        </Box>
-      </Stack>
-    </Container>
+        </Stack>
+      </Container>
+    </React.Fragment>
   );
 }
