3adf47e3a4547c070bf208804b4dd3f90bda5f8b
Codemode001 <haroldrex12331@gmail.com>
2022-05-04 20:39:52 +0800

    Bring back the deleted imports for routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
 import React from "react";
 import Navbar from "./pages/navbar";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
+import Signup from "./pages/signup";
+import LoginPage from "./pages/login";
 
 const App = () => {
   return (
     <BrowserRouter>
       <Switch>
         <Route exact path="/navbar" component={Navbar} />
+        <Route path="/" component={Signup} />
+        <Route exact path="/login" component={LoginPage} />
       </Switch>
     </BrowserRouter>
   );
