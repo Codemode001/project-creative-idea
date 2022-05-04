7b39b09d6e9ab6f0f4446b63c46c3788530e14b9
Codemode001 <haroldrex12331@gmail.com>
2022-05-04 17:01:04 +0800

    Update index.jsx

--- src/pages/navbar/index.jsx
+++ src/pages/navbar/index.jsx
@@ -23,10 +23,6 @@ import ExpandMore from "@mui/icons-material/ExpandMore";
 import ChatIcon from "@mui/icons-material/Chat";
 import { styled, alpha } from "@mui/material/styles";
 import Menu from "@mui/material/Menu";
-import EditIcon from "@mui/icons-material/Edit";
-import ArchiveIcon from "@mui/icons-material/Archive";
-import FileCopyIcon from "@mui/icons-material/FileCopy";
-import MoreHorizIcon from "@mui/icons-material/MoreHoriz";
 import KeyboardArrowDownIcon from "@mui/icons-material/KeyboardArrowDown";
 
 function notificationsLabel(count) {
